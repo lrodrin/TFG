56d525abc854afb9f00a059731652efa25995ae3
Laura <laura.rodriguez.navas@upc.edu>
2017-01-16 14:49:23 +0100

    afegits els comentaris per a la classe T

--- src/final/T.py
+++ src/final/T.py
@@ -16,63 +16,78 @@ __license__ = 'MIT'
 class T(object):
     @staticmethod
     def createTestructure(clansList):
+        """
+            Create a T-strcuture from a list of clans
+
+        :param clansList: List of clans
+        :type clansList: list of sets
+        :return: DOT file
+        """
         external, internal = c.Clan.division(clansList)
 
         callgraph = pydot.Dot(graph_type='digraph', compound=True)
 
-        # nodes
-        for e in external:
-            callgraph.add_node(pydot.Node("".join(e)))
+        # creating nodes
+        for external_nodes in external:  # For each clan that len() == 1
+            callgraph.add_node(pydot.Node("".join(external_nodes)))  # Add external node to file
 
+        # creating subgraphs
         i = len(internal) - 1
         j = i - 1
-        while i >= 0:
-            clusterName = pydot.Cluster("".join(internal[i]))
-            subgraph = pydot.Subgraph(rank="same")
+        while i >= 0:  # Run list of clans in reverse mode
+            cluster = pydot.Cluster("".join(internal[i]))  # Create a cluster cluster
+            internalSubgraph = pydot.Subgraph(rank="same")  # Create a subgraph from cluster cluster
             if i != 0:
-                clusterName.add_node(
+                cluster.add_node(
                     pydot.Node("s_%s" % "".join(internal[j]), label=" ", fillcolor="white", fixedsize=True,
-                               width=0.2))
-                clusterName.add_node(pydot.Node("s_%s" % "".join(internal[i].difference(internal[j])), label=" ",
-                                                fillcolor="white", fixedsize=True, width=0.2))
-                subgraph.add_edge(
+                               width=0.2))  # Add actual node/clan to cluster
+                cluster.add_node(pydot.Node("s_%s" % "".join(internal[i].difference(internal[j])), label=" ",
+                                            fillcolor="white", fixedsize=True, width=0.2))  # Add actual node/clan
+                # difference from internal actual clan and internal previous clan to cluster
+                internalSubgraph.add_edge(
                     pydot.Edge("s_%s" % "".join(internal[i].difference(internal[j])), "s_%s" % "".join(internal[j]),
-                               arrowhead="none"))
-            else:
-                internal_i_aux = internal[i].copy()
-                internal_i_aux2 = internal[i].copy()
-                clusterName.add_node(
+                               arrowhead="none"))  # Add edge from second node/clan to first node/clan in
+                # internalSubgraph difference from internal actual clan and internal previous clan to cluster
+            else:  # Last iteration
+                internal_i_aux = internal[i].copy()  # Copy from actual clan
+                internal_i_aux2 = internal[i].copy()  # A sedcond copy from actual clan
+                # Add nodes for each element from the last clan in internals
+                cluster.add_node(
                     pydot.Node("s_%s" % "".join(internal_i_aux.pop()), label=" ", fillcolor="white", fixedsize=True,
                                width=0.2))
-                clusterName.add_node(
+                cluster.add_node(
                     pydot.Node("s_%s" % "".join(internal_i_aux), label=" ", fillcolor="white", fixedsize=True,
                                width=0.2))
-                subgraph.add_edge(
+                # Add edge for nodes from the last clan in internals
+                internalSubgraph.add_edge(
                     pydot.Edge("s_%s" % "".join(internal_i_aux2.pop()), "s_%s" % "".join(internal_i_aux2),
                                arrowhead="none"))
 
-            clusterName.add_subgraph(subgraph)
-            callgraph.add_subgraph(clusterName)
+            cluster.add_subgraph(internalSubgraph)  # Add subgraph to cluster
+            callgraph.add_subgraph(cluster)  # Add cluster to file
             i -= 1
             j -= 1
 
-        # links
-        for e in external:
-            callgraph.add_edge(pydot.Edge("s_%s" % "".join(e), "".join(e), arrowhead="none"))
+        # creating links for nodes and subgraphs
+        for external_nodes in external:  # For each clan that len() == 1
+            callgraph.add_edge(pydot.Edge("s_%s" % "".join(external_nodes), "".join(external_nodes), arrowhead="none"))
+            # Add edge from subgraph node/clan to external node/clan
 
         i = len(internal) - 2
         j = i - 1
-        while i >= 0:
+        while i >= 0:  # Run list of clans in reverse mode
             if i != 0:
+                # Add edge from internal node/clan in subgraph to cluster
                 callgraph.add_edge(
                     pydot.Edge("s_%s" % "".join(internal[i]), "s_%s" % "".join(internal[j]), arrowhead="none",
                                lhead="cluster_%s" % "".join(internal[i])))
             else:
-                internal_i_aux = internal[i].copy()
+                # Add the last edge from internal node/clan in subgraph to cluster
+                internal_i_aux = internal[i].copy()  # Copy from actual clan
                 callgraph.add_edge(
                     pydot.Edge("s_%s" % "".join(internal[i]), "s_%s" % "".join(internal[i].pop()), arrowhead="none",
                                lhead="cluster_%s" % "".join(internal_i_aux)))
             i -= 1
             j -= 1
 
-        callgraph.write('T.dot')
+        callgraph.write('T.dot')  # Write a DOT file with previous information

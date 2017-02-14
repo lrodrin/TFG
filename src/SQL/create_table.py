6ce096e8bec5063e694e501c50722d9e0f658b42
Laura <laura.rodriguez.navas@upc.edu>
2017-02-14 14:50:03 +0100

    s'han afegit les opcions següents a create_table.sql: - try/exception - l'usuari dóna el path de la BD, data file i el nom de la taula nova - es creen automàticament les columnes a partir del fitxer de dades

--- src/SQL/create_table.py
+++ src/SQL/create_table.py
@@ -7,25 +7,62 @@ Distributed under MIT license
 [https://opensource.org/licenses/MIT]
 """
 import sqlite3
+
 import re
 
 __author__ = 'Laura Rodriguez Navas'
 __license__ = 'MIT'
 
-pathBD = str(input("Please enter a path from database file input: "))
-conn = sqlite3.connect(pathBD)
-# conn = sqlite3.connect('C:/Users/Laura/PycharmProjects/TFG/src/SQL/BD.db')
-c = conn.cursor()
+# global variables
+connection = ""
+file = ""
+query = ""
+tableName = ""
 
-pathFile = str(input("Please enter a path from data file input: "))
-file = open(pathFile, 'r')
-# file = open('C:/Users/Laura/PycharmProjects/TFG/src/SQL/meteo.txt', 'r')
-lines = file.readlines()
+try:
+    connection = sqlite3.connect('C:/Users/Laura/PycharmProjects/TFG/src/SQL/BD.db')  # Connection to database
+    # pathBD = str(input("Please enter a path from database file: "))
+    # connection = sqlite3.connect(pathBD)
+except sqlite3.Error as e:
+    print("Error:", e)
+
+cur = connection.cursor()
+
+try:
+    file = open('C:/Users/Laura/PycharmProjects/TFG/src/SQL/meteo.txt', 'r')  # Open data file
+    # pathFile = str(input("Please enter a path from data file: "))
+    # file = open(pathFile, 'r')
+except sqlite3.Error as e:
+    print("Error:", e)
+
+lines = file.readlines()  # Keep all lines of the data file into lines
+header = lines[0]  # Extract the header from data file
+colNames = ""
+colCounter = 0  # Counter to count the number of columns
+for word in header.split(" "):  # For each word from header keep a column in colNames
+    colNames += word + ", "  # Adding a column
+    colCounter += 1  # Increase the number of columns
 file.close()
 
-c.execute('CREATE TABLE IF NOT EXISTS test (outlook, temperature, humidity, windy, play)')
-for ns in lines:
-    d = re.split('\s+', ns, 4)
-    c.execute('INSERT OR IGNORE INTO test ( outlook, temperature, humidity, windy, play) VALUES (?, ?, ?, ? , ?)',
-              (d[0].split(':')[1], d[1].split(':')[1], d[2].split(':')[1], d[3].split(':')[1], d[4].split(':')[1]))
-    conn.commit()
+try:
+    tableName = str(input("Please enter a name for the database table: "))
+    query = "create table " + tableName + " (" + colNames[0:-2] + ")"  # Create the table if not exists
+    cur.execute(query)
+except sqlite3.Error as e:
+    print("Error:", e)
+
+# IF NOT EXISTS (SELECT termino FROM test2 WHERE Nombre=@Nombre AND curso=@Curso) INSERT test2 (Nombre, Curso, Termino)
+# VALUES (@Nombre, @Curso, @Termino)
+
+for line in lines:
+    value = re.split('\s+', line, colCounter)
+
+    query = "insert or ignore into " + tableName + " (" + colNames[0:-2] + ") values"
+    # cur.execute('INSERT OR IGNORE INTO test ( outlook, temperature, humidity, windy, play) VALUES (?, ?, ?, ? , ?)',
+    #             (d[0].split(':')[1], d[1].split(':')[1], d[2].split(':')[1], d[3].split(':')[1], d[4].split(':')[1]))
+    print(query)
+    #connection.commit()
+
+cur.close()
+connection.close()
+# TODO fusionar amb gaifman
--- src/SQL/gaifman.py
+++ src/SQL/gaifman.py
@@ -9,19 +9,24 @@ Distributed under MIT license
 import itertools
 import sqlite3
 import networkx as nx
-from src.final.Estructura import T
+import src.final.Estructura as e
 import src.iter_subsets as it
 import src.final.Clan as c
 
 __author__ = 'Laura Rodriguez Navas'
 __license__ = 'MIT'
 
-connection = sqlite3.connect('C:/Users/Laura/PycharmProjects/TFG/src/SQL/BD.db')
-cur = connection.cursor()
-cur.execute('SELECT * FROM test')
+pathBD = str(input("Please enter a path from database file input: "))
+connection = sqlite3.connect(pathBD)
+# connection = sqlite3.connect('C:/Users/Laura/PycharmProjects/TFG/src/SQL/BD.db')
+cursor = connection.cursor()
 
-colNames = [description[0] for description in cur.description]
-rows = cur.fetchall()
+# TODO creating table test
+cursor.execute('SELECT * FROM test')
+
+colNames = [description[0] for description in cursor.description]
+print(colNames)
+rows = cursor.fetchall()
 
 G = nx.Graph()
 
@@ -56,7 +61,8 @@ for row in rows:
             G.edge[u][v]['label'] = 1
 
 
-colors = {0: 'white', 1: 'black', 2: 'cyan', 3: 'green', 4: 'magenta', 5: 'orange', 6: 'purple', 7: 'red', 8: 'yellow', 9: 'brown'}
+colors = {0: 'white', 1: 'black', 2: 'cyan', 3: 'green', 4: 'magenta', 5: 'orange', 6: 'purple', 7: 'red', 8: 'yellow',
+          9: 'brown'}
 for key, value in colors.items():
     for u, v in G.edges():
         if 'label' in G[u][v] and key == G[u][v]['label']:
@@ -70,11 +76,11 @@ for s in it.powerset_generator(set(G.nodes())):  # Subset iterator of each subse
     if c.Clan.clans(G, s):  # If s is a clan of G
         clansList.append(s)  # Add s to the list
 
-edgesAtributtes = nx.get_edge_attributes(G,'color')
+edgesAtributtes = nx.get_edge_attributes(G, 'color')
 print(edgesAtributtes)
 print(clansList)
-T.create_2structure(clansList, edgesAtributtes)
+e.Estructura.create_2structure(clansList, edgesAtributtes)
 
 
-cur.close()
+cursor.close()
 connection.close()

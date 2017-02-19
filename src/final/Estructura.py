22e3e4d3f626901bb470b55e41c59d439df8a7b1
Laura <laura.rodriguez.navas@upc.edu>
2017-02-19 19:39:35 +0100

    creació de les 2-structures dels exemples propis.

--- src/final/Estructura.py
+++ src/final/Estructura.py
@@ -23,8 +23,10 @@ class Estructura(object):
 
         :param graphEdgesAtributtes: Dictionary of edges atributtes from a Graph
         :param primalsDict: List of primal clans
+        :param filename: DOT file name
         :type graphEdgesAtributtes: dict
         :type primalsDict: dict of frozenset
+        :type filename: str
         :return: A 2-structure
         :rtype: DOT file
         """
@@ -46,7 +48,7 @@ class Estructura(object):
 
             for pair in itertools.combinations(value, 2):  # For each pair of combinations from primal clan values
                 subgraph.add_edge(pydot.Edge("s_%s" % "".join(pair[0]), "s_%s" % "".join(pair[1]), arrowhead="none",
-                                             color=Estructura.getColorClans(graphEdgesAtributtes, pair[0], pair[1])))
+                                             color=getColorClans(graphEdgesAtributtes, pair[0], pair[1])))
                 #  Add edge into subgraph
             cluster.add_subgraph(subgraph)  # Add subgraph to cluster
             callgraph.add_subgraph(cluster)  # Add cluster to DOT file
@@ -59,49 +61,49 @@ class Estructura(object):
                         pydot.Edge("s_%s" % "".join(elem), "".join(elem), arrowhead="none"))  # Add primal clan
                     # values as a edge
 
-        for i, (key, value) in enumerate(primalsDict.items()):
-            if i != 0:
+        for i, (key, value) in enumerate(primalsDict.items()):  # For each primal clan
+            if i != 0:  # If not the first primal clan in primalsDict
                 callgraph.add_edge(pydot.Edge("s_%s" % "".join(key), "s_%s" % "".join(value[0]),
-                                              arrowhead="none", lhead="cluster_%s" % "".join(key)))
+                                              arrowhead="none", lhead="cluster_%s" % "".join(key)))  # Add primal clan
+                # values as a edge
 
         callgraph.write(filename)  # Write a DOT file with all previous information
 
-    @staticmethod
-    def getColorClans(graphEdgesAtributtes, primalClan_1, primalClan_2):
-        """
-            Get the color edge between two primal clans
 
-        :param graphEdgesAtributtes: Dictionary of edges atributtes from a Graph
-        :param primalClan_1: Primal clan
-        :param primalClan_2: Primal clan
-        :type graphEdgesAtributtes: dict
-        :type primalClan_1: set
-        :type primalClan_2: set
-        :return: Color edge between primalClan_1 and primalClan_2
-        :rtype: str
-        """
-        for key, value in graphEdgesAtributtes.items():  # For each primal clan
-            if (key[0] in primalClan_1 and key[1] in primalClan_2) or (key[1] in primalClan_1 and key[0] in
-            primalClan_2):
-                return value
+def primalSubsets(primalsList):
+    """
+        Return a dictionary with all subsets from a primal clans list
 
-    @staticmethod
-    def primalSubsets(primalsList):
-        """
-            Return a dictionary with all subsets from a primal clans list
+    :param primalsList: List of primal clans
+    :type primalsList: list
+    :return: A dictionary with the subsets from primalsList
+    :rtype: dict
+    """
+    dictionary = defaultdict(list)  # Empty dictionary
+    for i in range(len(primalsList) - 1, 0, -1):
+        for j in range(i - 1, -1, -1):
+            if primalsList[j].issubset(primalsList[i]):
+                for k in range(j + 1, i):
+                    if primalsList[k].issubset(primalsList[i]) and primalsList[j].issubset(primalsList[k]):
+                        break
+                else:
+                    dictionary[frozenset(primalsList[i])].append(primalsList[j])
+    return dictionary
 
-        :param primalsList: List of primal clans
-        :type primalsList: list
-        :return: A dictionary with the subsets from primalsList
-        :rtype: dict
-        """
-        dictionary = defaultdict(list)   # Empty dictionary
-        for i in range(len(primalsList) - 1, 0, -1):
-            for j in range(i - 1, -1, -1):
-                if primalsList[j].issubset(primalsList[i]):
-                    for k in range(j + 1, i):
-                        if primalsList[k].issubset(primalsList[i]) and primalsList[j].issubset(primalsList[k]):
-                            break
-                    else:
-                        dictionary[frozenset(primalsList[i])].append(primalsList[j])
-        return dictionary
+
+def getColorClans(graphEdgesAtributtes, primalClan_1, primalClan_2):
+    """
+        Get the color edge between two primal clans
+
+    :param graphEdgesAtributtes: Dictionary of edges atributtes from a Graph
+    :param primalClan_1: Primal clan
+    :param primalClan_2: Primal clan
+    :type graphEdgesAtributtes: dict
+    :type primalClan_1: set
+    :type primalClan_2: set
+    :return: Color edge between primalClan_1 and primalClan_2
+    :rtype: str
+    """
+    for key, value in graphEdgesAtributtes.items():  # For each primal clan
+        if (key[0] in primalClan_1 and key[1] in primalClan_2) or (key[1] in primalClan_1 and key[0] in primalClan_2):
+            return value
--- src/final/mainEstructura.py
+++ src/final/mainEstructura.py
@@ -7,19 +7,19 @@ Distributed under MIT license
 [https://opensource.org/licenses/MIT]
 """
 from collections import OrderedDict
+
 import src.final.Estructura as e
 
 __author__ = 'Laura Rodriguez Navas'
 __license__ = 'MIT'
 
 if __name__ == "__main__":
-
     primalsList_1 = [frozenset({'B'}), frozenset({'A'}), frozenset({'D'}), frozenset({'C'}), frozenset({'E'}),
                      frozenset({'E', 'D'}), frozenset({'A', 'E', 'D'}), frozenset({'A', 'E', 'D', 'B'}),
-                     frozenset({'A', 'E', 'D', 'C', 'B'})]
+                     frozenset({'A', 'E', 'D', 'C', 'B'})]  # First primal clans list
     primalsList_2 = [frozenset({'C'}), frozenset({'A'}), frozenset({'D'}), frozenset({'F'}), frozenset({'E'}),
                      frozenset({'B'}), frozenset({'D', 'C'}), frozenset({'B', 'A'}),
-                     frozenset({'D', 'B', 'E', 'F', 'C', 'A'})]
+                     frozenset({'D', 'B', 'E', 'F', 'C', 'A'})]  # Second primal clans list
 
     edgesAtributtesfromGraph_1 = {('C', 'B'): 'black', ('B', 'A'): 'red', ('D', 'A'): 'blue', ('E', 'A'): 'blue',
                                   ('C', 'A'): 'black', ('E', 'C'): 'black', ('D', 'B'): 'red', ('E', 'B'): 'red',
@@ -29,19 +29,20 @@ if __name__ == "__main__":
                                   ('A', 'C'): 'black', ('E', 'C'): 'orange', ('D', 'E'): 'orange', ('D', 'F'): 'blue',
                                   ('A', 'F'): 'green', ('E', 'F'): 'red', ('B', 'F'): 'green'}
 
-    primalsDict_1 = OrderedDict(reversed(sorted(e.Estructura.primalSubsets(primalsList_1).items(),
-                                                key=lambda t: len(t[0]))))
+    primalsDict_1 = OrderedDict(reversed(sorted(e.primalSubsets(primalsList_1).items(),
+                                                key=lambda t: len(t[0]))))  # dictionary sorted by length of the
+    # key string
     print("First primals list:\n", primalsDict_1)
 
-    primalsDict_2 = OrderedDict(reversed(sorted(e.Estructura.primalSubsets(primalsList_2).items(),
+    primalsDict_2 = OrderedDict(reversed(sorted(e.primalSubsets(primalsList_2).items(),
                                                 key=lambda t: len(t[0]))))
     print("Second primals list:\n", primalsDict_2)
     print("-" * 20)
 
-    print("Get color from {'E', 'A', 'D', 'B'} and {'C'}:", (e.Estructura.getColorClans(edgesAtributtesfromGraph_1,
-                                                                                        {'E', 'A', 'D', 'B'}, {'C'})))
-    print("Get color from {'A', 'B'} and {'F'}:", (e.Estructura.getColorClans(edgesAtributtesfromGraph_2,
-                                                                              {'A', 'B'}, {'F'})))
+    print("Get color from {'E', 'A', 'D', 'B'} and {'C'}:", (e.getColorClans(edgesAtributtesfromGraph_1,
+                                                                             {'E', 'A', 'D', 'B'}, {'C'})))
+    print("Get color from {'A', 'B'} and {'F'}:", (e.getColorClans(edgesAtributtesfromGraph_2,
+                                                                   {'A', 'B'}, {'F'})))
     print("-" * 20)
 
     # print("Creating a 2-structure...")

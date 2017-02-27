2349d307ab34dd1e413aaa0d06f69cbf8e3e1287
Laura <laura.rodriguez.navas@upc.edu>
2017-02-27 19:33:26 +0100

    s'obren els fitxers .dot automàticament des de la implementació / des de Pycharm

---
+++ src/graphviz/run_external_program.py
@@ -0,0 +1,15 @@
+"""
+This module implements the call for Graphviz program
+
+Copyright (c) 2016-2017 Laura Rodriguez Navas <laura.rodriguez.navas@upc.edu>
+
+Distributed under MIT license
+[https://opensource.org/licenses/MIT]
+"""
+import os
+
+__author__ = 'Laura Rodriguez Navas'
+__license__ = 'MIT'
+
+
+os.startfile('graphviz_4.dot')

14c8bb61add341ec617e642b8cd2e50946de2603
Laura <laura.rodriguez.navas@upc.edu>
2017-03-06 21:24:09 +0100

    tests a Clan i Estructura

--- src/final/mainClan.py
+++ src/final/mainClan.py
@@ -19,10 +19,10 @@ if __name__ == "__main__":
     graph.add_edges_from([('A', 'C'), ('B', 'C'), ('C', 'D'), ('C', 'E')], color='black')
     graph.add_edges_from([('A', 'D'), ('A', 'E')], color='blue')
 
-    clansList = Clan.clans(graph, graph.nodes())
+    clansList = Clan.clans(graph, set(graph.nodes()))
     print("List of clans:\n", clansList)
 
-    trivialClansList = Clan.trivialClans(graph.nodes(), nx.graph_clique_number(graph))
+    trivialClansList = Clan.trivialClans(set(graph.nodes()), nx.graph_clique_number(graph))
     print("List of trivial clans:\n", trivialClansList)
 
     primalClansList = Clan.primalClans(clansList)
@@ -34,10 +34,10 @@ if __name__ == "__main__":
     graph, rows = Graph.createGraph('C:/Users/Laura/PycharmProjects/TFG/src/SQL/BD.db', 'test')
     plainGraph = Graph.createPlainGraph(graph, rows)
 
-    clansList = Clan.clans(plainGraph, plainGraph.nodes())
+    clansList = Clan.clans(plainGraph, set(plainGraph.nodes()))
     print("List of clans:\n", clansList)
 
-    trivialClansList = Clan.trivialClans(plainGraph.nodes(), nx.graph_clique_number(plainGraph))
+    trivialClansList = Clan.trivialClans(set(plainGraph.nodes()), nx.graph_clique_number(plainGraph))
     print("List of trivial clans:\n", trivialClansList)
 
     primalClansList = Clan.primalClans(clansList)

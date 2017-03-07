36024a6ebde99b067b867fcec4f03622dcfc228a
Laura <laura.rodriguez.navas@upc.edu>
2017-03-07 16:43:21 +0100

    backup

--- src/final/mainClan.py
+++ src/final/mainClan.py
@@ -15,14 +15,14 @@ __license__ = 'MIT'
 if __name__ == "__main__":
     graph = nx.Graph()  # Create an empty graph structure (a “null graph”) with no nodes and no edges
     # Adding edges and edges attributes
-    graph.add_edges_from([('A', 'B'), ('B', 'D'), ('B', 'E'), ('D', 'E')], color='red')
+    graph.add_edges_from([('A', 'B'), ('B', 'D'), ('B', 'E'), ('D', 'E')], color='white')
     graph.add_edges_from([('A', 'C'), ('B', 'C'), ('C', 'D'), ('C', 'E')], color='black')
     graph.add_edges_from([('A', 'D'), ('A', 'E')], color='blue')
 
-    clansList = Clan.clans(graph, set(graph.nodes()))
+    clansList = Clan.clans(graph, graph.nodes())
     print("List of clans:\n", clansList)
 
-    trivialClansList = Clan.trivialClans(set(graph.nodes()), nx.graph_clique_number(graph))
+    trivialClansList = Clan.trivialClans(graph.nodes(), nx.graph_clique_number(graph))
     print("List of trivial clans:\n", trivialClansList)
 
     primalClansList = Clan.primalClans(clansList)

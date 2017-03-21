b5d87c61d32d935136f9d521760b61c281113bcf
Laura <laura.rodriguez.navas@upc.edu>
2017-03-21 21:10:04 +0100

    refactoring Interface + mainInterface

--- src/final/Interface.py
+++ src/final/Interface.py
@@ -23,117 +23,126 @@ class Interface:
         Create and display different types of graphs: plain, plain with threshold, linear and exponential
 
         :param option: Option graph
-        :param graph: NetworkX's Graph
-        :param rows: Rows from a table
+        :param graph: Networkx's Graph
+        :param rows: Rows from a SQLite table
         :type option: int
         :type graph: nx.Graph
         """
         if option == 1:
-            Graph.createPlainGraph(graph, rows)
-            filename = 'plainGraph.dot'
-            Graph.exportGraphDOT(graph, filename)
-            Interface.openGraphviz(filename)
+            Graph.createPlainGraph(graph, rows)  # Create a plain graph
+            graphName = "plainGraph.dot"
+            Graph.exportGraphDOT(graph, graphName)  # Export graph to Graphviz format
         elif option == 2:
-            Graph.createLinearGraph(graph, rows)
-            filename = 'linearGraph.dot'
-            Graph.exportGraphDOT(graph, filename)
-            Interface.openGraphviz(filename)
-        elif option == 3:
-            Graph.createExponentialGraph(graph, rows)
-            filename = 'exponentialGraph.dot'
-            Graph.exportGraphDOT(graph, filename)
-            Interface.openGraphviz(filename)
-        elif option == 4:
             threshold = int(input("Please enter the k constant for the threshold:\n"))
-            Graph.createPlainGraphWithThreshold(graph, rows, threshold)
-            filename = 'plainGraphWithThreshold.dot'
-            Graph.exportGraphDOT(graph, filename)
-            Interface.openGraphviz(filename)
+            Graph.createPlainGraphWithThreshold(graph, rows, threshold)  # Create a plain graph with threshold
+            graphName = "plainGraphWithThreshold.dot"
+            Graph.exportGraphDOT(graph, graphName)  # Export graph to Graphviz format
+        elif option == 3:
+            Graph.createLinearGraph(graph, rows)  # Create a linear graph
+            graphName = "linearGraph.dot"
+            Graph.exportGraphDOT(graph, graphName)  # Export graph to Graphviz format
+        elif option == 4:
+            Graph.createExponentialGraph(graph, rows)  # Create an exponential graph
+            graphName = "exponentialGraph.dot"
+            Graph.exportGraphDOT(graph, graphName)  # Export graph to Graphviz format
+
+        Interface.openGraphviz(graphName)  # Open graph in Graphviz program
 
     @staticmethod
     def structureOptions(option, graph, rows):
         """
-        Create and display different types of graphs and 2-structures: plain, linear and exponential
-        :param option: Option graph
-        :param graph: NetworkX's Graph
-        :param rows: Rows from a table
+        Create and display different types of 2-structures: plain, plain with threshold, linear and exponential
+
+        :param option: Option 2-structure
+        :param graph: Networkx's Graph
+        :param rows: Rows from a SQLite table
         :type option: int
         :type graph: nx.Graph
         """
         if option == 1:
-            optionPlain = int(input("Please enter the option of plain graph you want to create:\n [1] = plain\n [2] = "
-                                    "plain with threshold\n"))
+            optionPlain = int(input(
+                "Please enter the option of graph you want to create:\n [1] = plain\n [2] = plain with threshold\n"))
             if optionPlain == 1:
-                plainGraph = Graph.createPlainGraph(graph, rows)
+                structureName = "plain 2-structure.dot"
+                plainGraph = Graph.createPlainGraph(graph, rows)  # Create a plain graph
             elif optionPlain == 2:
+                structureName = "plain 2-structure with threshold.dot"
                 threshold = int(input("Please enter the k constant for the threshold:\n"))
-                plainGraph = Graph.createPlainGraphWithThreshold(graph, rows, threshold)
-            Estructura.plain2structure(plainGraph, plainGraph.nodes())
-            Interface.openGraphviz('plain2-structure.dot')
+                plainGraph = Graph.createPlainGraphWithThreshold(graph, rows,
+                                                                 threshold)  # Create a plain graph with threshold
+
+            Estructura.plain2structure(plainGraph, plainGraph.nodes(), structureName)  # Create a plain 2-structure
+
         elif option == 2:
+            structureName = "linear 2-structure.dot"
             linearGraph = Graph.createLinearGraph(graph, rows)
-            Estructura.linear2structure(linearGraph, linearGraph.nodes())
-            Interface.openGraphviz('linear2-structure.dot')
+            Estructura.linear2structure(linearGraph, linearGraph.nodes(), structureName)  # Create a linear 2-structure
         elif option == 3:
+            structureName = "exponential 2-structure.dot"
             exponentialGraph = Graph.createExponentialGraph(graph, rows)
-            Estructura.exponential2structure(exponentialGraph, exponentialGraph.nodes())
-            Interface.openGraphviz('exponential2-structure.dot')
+            Estructura.exponential2structure(exponentialGraph, exponentialGraph.nodes(),
+                                             structureName)  # Create an exponential 2-structure
+
+        Interface.openGraphviz(structureName)  # Open 2-structure in Graphviz program
 
     @staticmethod
-    def graphAndstructureOptions(option, graph, rows):
+    def graphANDstructureOptions(option, graph, rows):
         """
-        Create and display different types of 2-structures: plain, linear and exponential
+        Create and display different types of graph and 2-structures: plain, plain with threshold, linear and exponential
 
-        :param option: Option graph
-        :param graph: NetworkX's Graph
-        :param rows: Rows from a table
+        :param option: Option graph and 2-structure
+        :param graph: Networkx's Graph
+        :param rows: Rows from a SQLite table
         :type option: int
         :type graph: nx.Graph
         """
         if option == 1:
-            optionPlain = int(input("Please enter the option of plain graph you want to create:\n [1] = plain\n [2] = "
-                                    "plain with threshold\n"))
+            optionPlain = int(input(
+                "Please enter the option of graph you want to create:\n [1] = plain\n [2] = plain with threshold\n"))
             if optionPlain == 1:
-                plainGraph = Graph.createPlainGraph(graph, rows)
-                filename = 'plainGraph.dot'
-                Graph.exportGraphDOT(graph, filename)
-                Interface.openGraphviz(filename)
+                graphName = "plainGraph.dot"
+                structureName = "plain 2-structure.dot"
+                plainGraph = Graph.createPlainGraph(graph, rows)  # Create a plain graph
+                Graph.exportGraphDOT(graph, graphName)  # Export graph to Graphviz format
+                Estructura.plain2structure(plainGraph, plainGraph.nodes(), structureName)  # Create a plain 2-structure
             elif optionPlain == 2:
+                graphName = "plainGraph with threshold.dot"
+                structureName = "plain 2-structure with threshold.dot"
                 threshold = int(input("Please enter the k constant for the threshold:\n"))
-                plainGraph = Graph.createPlainGraphWithThreshold(graph, rows, threshold)
-                filename = 'plainGraphWithThreshold.dot'
-                Graph.exportGraphDOT(graph, filename)
-                Interface.openGraphviz(filename)
-            Estructura.plain2structure(plainGraph, plainGraph.nodes())
-            Interface.openGraphviz('plain2-structure.dot')
+                plainGraph = Graph.createPlainGraphWithThreshold(graph, rows, threshold)  # Create a plain graph with
+                #  threshold
+                Graph.exportGraphDOT(graph, graphName)  # Export graph to Graphviz format
+                Estructura.plain2structure(plainGraph, plainGraph.nodes(), structureName)  # Create a plain 2-structure
+
         elif option == 2:
-            linearGraph = Graph.createLinearGraph(graph, rows)
-            filename = 'linearGraph.dot'
-            Graph.exportGraphDOT(graph, filename)
-            Interface.openGraphviz(filename)
-            Estructura.linear2structure(linearGraph, linearGraph.nodes())
-            Interface.openGraphviz('linear2-structure.dot')
+            graphName = "linearGraph.dot"
+            structureName = "linear 2-structure.dot"
+            linearGraph = Graph.createLinearGraph(graph, rows)  # Create a linear graph
+            Graph.exportGraphDOT(graph, graphName)  # Export graph to Graphviz format
+            Estructura.linear2structure(linearGraph, linearGraph.nodes(), structureName)
         elif option == 3:
-            exponentialGraph = Graph.createExponentialGraph(graph, rows)
-            filename = 'exponentialGraph.dot'
-            Graph.exportGraphDOT(graph, filename)
-            Interface.openGraphviz(filename)
-            Estructura.exponential2structure(exponentialGraph, exponentialGraph.nodes())
-            Interface.openGraphviz('exponential2-structure.dot')
+            graphName = "exponentialGraph.dot"
+            structureName = "exponential 2-structure.dot"
+            exponentialGraph = Graph.createExponentialGraph(graph, rows)  # Create an exponential graph
+            Graph.exportGraphDOT(graph, graphName)  # Export graph to Graphviz format
+            Estructura.exponential2structure(exponentialGraph, exponentialGraph.nodes(), structureName)
+
+        Interface.openGraphviz(graphName)   # Open graph in Graphviz program
+        Interface.openGraphviz(structureName)  # Open 2-structure in Graphviz program
 
     @staticmethod
     def fileOptions(option):
         if option == 1:
-            file = str(input("Please enter the name from ARFF data file:\n"))
-            # connection, cursor = Data.connection(file[0:-5] + ".db")  # Connection to SQLite database
-            connection, cursor = Data.connection("DB" + ".db")  # Connection to SQLite database
+            file = str(input("Please enter the name from arff file:\n"))
+            connection, cursor = Data.connection(file[0:-5] + ".db")  # Connection to SQLite database
             file = Data.openFile(file)  # Open data file
-            columnNames, lines = Data.getDataARFFile(file)  # Get column names and lines from file
-            tableName = Data.getTableNameFromARFFFile(lines)
+            columnNames, lines = Data.getDataARFFile(file)  # Get column names and lines from data file
+            tableName = Data.getTableNameFromARFFFile(lines)  # Get table name from data file
             Data.createTableARFF(cursor, tableName, columnNames)  # Create table tableName
-            Data.insertARFF(tableName, columnNames, lines, cursor, connection)  # Insert data to tableName
+            Data.insertARFF(tableName, columnNames, lines, cursor, connection)  # Insert data values to tableName
+
         elif option == 2:
-            file = str(input("Please enter the name from DB SQLite file:\n"))
+            file = str(input("Please enter the name from SQLite file:\n"))
             connection, cursor = Data.connection(file)  # Connection to SQLite database
 
         return cursor
@@ -141,9 +150,9 @@ class Interface:
     @staticmethod
     def openGraphviz(filename):
         """
-        Call the Graphviz program that is associated with a DOT file
+        Call the Graphviz program that is associated with a Dot file specified by filename
 
-        :param filename: DOT file
+        :param filename: Dot file
         :type filename: str
         """
         if sys.platform == 'win32':  # Windows platform
--- src/final/mainInterface.py
+++ src/final/mainInterface.py
@@ -15,13 +15,14 @@ if __name__ == '__main__':
     fileOption = int(input("Please enter the option for the file you provide:\n [1] = .arff\n [2] = .db\n"))
     cursor = Interface.fileOptions(fileOption)
 
-    tableName = str(input("Table name: \n"))
-    graph, rows = Graph.initializeGraph(tableName, cursor)
+    tableName = str(input("Please enter the table name: \n"))
+    graph, rows = Graph.initializeGraph(tableName, cursor)  # Initialize the graph
 
-    # option = int(input("Please enter the option of graph you want to create:\n [1] = plain\n [2] = linear\n [3] "
-    # "= exponential\n [4] = plain with threshold\n")) Interface.graphOptions(option, graph, rows) option = int(
-    # input("Please enter the option of 2-structure you want to create:\n [1] = plain\n [2] = linear\n [3] " "=
-    # exponential\n")) Interface.structureOptions(option, graph, rows)
-    option = int(input("Please enter the option of graph and structure you want to create:\n [1] = plain\n [2] = "
+    # option = int(input("Please enter the option of graph you want to create:\n [1] = plain\n [2] = plain with "
+    #                    "threshold\n [3] = linear\n [4] = exponential\n"))
+    # Interface.graphOptions(option, graph, rows)
+
+    option = int(input("Please enter the option of 2-structure you want to create:\n [1] = plain\n [2] = "
                        "linear\n [3] = exponential\n"))
-    Interface.graphAndstructureOptions(option, graph, rows)
+    # Interface.structureOptions(option, graph, rows)
+    Interface.graphANDstructureOptions(option, graph, rows)

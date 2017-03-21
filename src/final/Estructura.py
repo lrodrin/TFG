a8119bab8328ea61232cd2e07584968ff4db4acb
Laura <laura.rodriguez.navas@upc.edu>
2017-03-21 18:21:00 +0100

    refactoring Estructura + mainEstructura

--- src/final/Estructura.py
+++ src/final/Estructura.py
@@ -17,30 +17,27 @@ __author__ = 'Laura Rodriguez Navas'
 __license__ = 'MIT'
 
 
-# TODO modificar la mida de cada cluster i els nodes
-
-
 class Estructura:
     @staticmethod
     def create2structure(EdgesAtributtes, primalClansDict, filename):
+        # TODO modificar la mida de cada cluster i els nodes
         """
-        Create a 2-structure from a list of primal clans specified by primalClansDict
+        Create a 2-structure from a dictionary of primal clans specified by primalClansDict
 
         :param EdgesAtributtes: Edges atributtes from a graph
-        :param primalClansDict: List of primal clans
-        :param filename: DOT file name
+        :param primalClansDict: Dictionary of primal clans
+        :param filename: Dot file name
         :type EdgesAtributtes: dict
         :type primalClansDict: dict
         :type filename: str
         :return: A 2-structure
-        :rtype: DOT file
         """
         callgraph = pydot.Dot(graph_type='digraph', compound=True)
 
-        # creating external nodes
-        for value in primalClansDict.values():  # For each primal clan values
+        # creating an external nodes
+        for value in primalClansDict.values():  # For each primal clan
             for elem in value:
-                if len(elem) == 1:  # If primal clan value is len() == 1
+                if len(elem) == 1:  # If len(primal clan) == 1
                     callgraph.add_node(pydot.Node("".join(elem)))  # Add primal clan value as a node
 
         # creating subgraphs
@@ -49,52 +46,97 @@ class Estructura:
             subgraph = pydot.Subgraph(rank="same")  # Create a subgraph into cluster
             for elem in value:
                 cluster.add_node(pydot.Node("s_%s" % "".join(elem), label=" ", fillcolor="white",
-                                            fixedsize=True, width=0.2))  # Add primal clan value as a node into cluster
+                                            fixedsize=True, width=0.2))  # Add primal clan as a node into cluster
 
-            for pair in itertools.combinations(value, 2):  # For each pair of combinations from primal clan values
+            for pair in itertools.combinations(value, 2):  # For each pair primal clan combinations
                 subgraph.add_edge(pydot.Edge("s_%s" % "".join(pair[0]), "s_%s" % "".join(pair[1]), arrowhead="none",
                                              color=Clan.getColorClans(EdgesAtributtes, pair[0], pair[1])))
                 #  Add edge into subgraph
             cluster.add_subgraph(subgraph)  # Add subgraph to cluster
-            callgraph.add_subgraph(cluster)  # Add cluster to DOT file
+            callgraph.add_subgraph(cluster)  # Add cluster
 
         # creating edge links for nodes and subgraphs
-        for value in primalClansDict.values():  # For each primal clan values
+        for value in primalClansDict.values():  # For each primal clan
             for elem in value:
-                if len(elem) == 1:  # If primal clan value is len() == 1
+                if len(elem) == 1:  # If len(primal clan) == 1
                     callgraph.add_edge(
                         pydot.Edge("s_%s" % "".join(elem), "".join(elem), arrowhead="none"))  # Add primal clan
-                    # values as a edge
+                    # value as a edge
 
         for i, (key, value) in enumerate(primalClansDict.items()):  # For each primal clan
             if i != 0:  # If not the first primal clan in primalClansDict
                 callgraph.add_edge(pydot.Edge("s_%s" % "".join(key), "s_%s" % "".join(value[0]),
                                               arrowhead="none", lhead="cluster_%s" % "".join(key)))  # Add primal clan
-                # values as a edge
+                # value as a edge
 
-        callgraph.write(filename)  # Write a DOT file with all previous information
+        callgraph.write(filename)  # Write a Dot file with all previous information
         print("2-structure %s was created" % filename)
 
     @staticmethod
     def decomposition(graph, nodes):
-        clansList = Clan.clans(graph, nodes)
-        primalClansList = Clan.primalClans(clansList)
-        EdgesAtributtes = Graph.createDictFromGraph(graph)
+        """
+        Decomposition of graph in clans and primal clans
+
+        :param graph: Networkx's graph
+        :param nodes: Nodes from graph
+        :type graph: nx.Graph
+        :type nodes: list
+        :return: The edges atributtes from graph and a dictionary of primal clans
+        :rtype: dict, dict
+        """
+        clansList = Clan.clans(graph, nodes)  # List of clans
+        primalClansList = Clan.primalClans(clansList)  # List of primal clans
+        EdgesAtributtes = Graph.createDictFromGraph(graph)  # Dictionary of edges atributtes from graph
         primalClansDict = OrderedDict(reversed(sorted(Clan.primalClansDict(primalClansList).items(),
-                                                      key=lambda t: len(t[0]))))
+                                                      key=lambda t: len(t[0]))))  # Dictionary of primal clans
+        # sorted in reverse mode by primal clans length
         return EdgesAtributtes, primalClansDict
 
     @staticmethod
-    def plain2structure(plainGraph, nodes):
-        EdgesAtributtes, primalClansDict = Estructura.decomposition(plainGraph, nodes)
-        Estructura.create2structure(EdgesAtributtes, primalClansDict, 'plain2-structure.dot')
+    def plain2structure(plainGraph, nodes, filename):
+        """
+        Create a plain 2-structure from a plain graph specified by plainGraph
+
+        :param plainGraph: Networkx's graph
+        :param nodes: Nodes from graph
+        :param filename: Dot file name
+        :type plainGraph: nx.Graph
+        :type nodes: list
+        :type filename: str
+        :return: A plain 2-structure
+        """
+        EdgesAtributtes, primalClansDict = Estructura.decomposition(plainGraph, nodes)  # Decomposition
+        Estructura.create2structure(EdgesAtributtes, primalClansDict, filename)  # Create 2-structure
 
     @staticmethod
-    def linear2structure(linearGraph, nodes):
-        EdgesAtributtes, primalClansDict = Estructura.decomposition(linearGraph, nodes)
-        Estructura.create2structure(EdgesAtributtes, primalClansDict, 'linear2-structure.dot')
+    def linear2structure(linearGraph, nodes, filename):
+        """
+        Create a linear 2-structure from a linear graph specified by linearGraph
+
+        :param linearGraph: Networkx's graph
+        :param nodes: Nodes from graph
+        :param filename: Dot file name
+        :type linearGraph: nx.Graph
+        :type nodes: list
+        :type filename: str
+        :return: A linear 2-structure
+        """
+        EdgesAtributtes, primalClansDict = Estructura.decomposition(linearGraph, nodes)  # Decomposition
+        Estructura.create2structure(EdgesAtributtes, primalClansDict, filename)  # Create 2-structure
 
     @staticmethod
-    def exponential2structure(exponentialGraph, nodes):
-        EdgesAtributtes, primalClansDict = Estructura.decomposition(exponentialGraph, nodes)
-        Estructura.create2structure(EdgesAtributtes, primalClansDict, 'exponential2-structure.dot')
+    def exponential2structure(exponentialGraph, nodes, filename):
+        """
+        Create an exponential 2-structure from a exponential graph specified by exponentialGraph
+
+        :param exponentialGraph: Networkx's graph
+        :param nodes: Nodes from graph
+        :param filename: Dot file name
+        :type exponentialGraph: nx.Graph
+        :type nodes: list
+        :type filename: str
+        :return: A exponential 2-structure
+        """
+        EdgesAtributtes, primalClansDict = Estructura.decomposition(exponentialGraph, nodes)  # Decomposition
+        Estructura.create2structure(EdgesAtributtes, primalClansDict, filename)  # Create
+        # 2-structure
--- src/final/mainEstructura.py
+++ src/final/mainEstructura.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     # primalsList_1 = [frozenset({'B'}), frozenset({'A'}), frozenset({'D'}), frozenset({'C'}), frozenset({'E'}),
     #                  frozenset({'E', 'D'}), frozenset({'A', 'E', 'D'}), frozenset({'A', 'E', 'D', 'B'}),
     #                  frozenset({'A', 'E', 'D', 'C', 'B'})]
+    #
     # primalsList_2 = [frozenset({'C'}), frozenset({'A'}), frozenset({'D'}), frozenset({'F'}), frozenset({'E'}),
     #                  frozenset({'B'}), frozenset({'D', 'C'}), frozenset({'B', 'A'}),
     #                  frozenset({'D', 'B', 'E', 'F', 'C', 'A'})]
@@ -22,6 +23,7 @@ if __name__ == "__main__":
     # edgesAtributtesfromGraph_1 = {('C', 'B'): 'black', ('B', 'A'): 'red', ('D', 'A'): 'blue', ('E', 'A'): 'blue',
     #                               ('C', 'A'): 'black', ('E', 'C'): 'black', ('D', 'B'): 'red', ('E', 'B'): 'red',
     #                               ('E', 'D'): 'red', ('C', 'D'): 'black'}
+    #
     # edgesAtributtesfromGraph_2 = {('D', 'C'): 'red', ('B', 'C'): 'black', ('F', 'C'): 'blue', ('D', 'A'): 'black',
     #                               ('E', 'A'): 'black', ('B', 'A'): 'black', ('D', 'B'): 'black', ('B', 'E'): 'black',
     #                               ('A', 'C'): 'black', ('E', 'C'): 'orange', ('D', 'E'): 'orange', ('D', 'F'): 'blue',
@@ -32,28 +34,28 @@ if __name__ == "__main__":
     # primalsDict_2 = OrderedDict(reversed(sorted(Clan.primalClansDict(primalsList_2).items(),
     #                                             key=lambda t: len(t[0]))))
     #
-    # print("First primals list:\n", primalsDict_1)
-    # print("Second primals list:\n", primalsDict_2)
+    # print("First primal clans dict:\n", primalsDict_1)
+    # print("Second primal clans dict:\n", primalsDict_2)
     #
     # Estructura.create2structure(edgesAtributtesfromGraph_1, primalsDict_1, 'Estructura.dot')
     # Estructura.create2structure(edgesAtributtesfromGraph_2, primalsDict_2, 'Estructura2.dot')
 
     fileOption = int(input("Please enter the option for the file you provide:\n [1] = .arff\n [2] = .db\n"))
     if fileOption == 1:
-        file = str(input("Please enter the name from ARFF data file:\n"))
-        # connection, cursor = Data.connection(file[0:-5] + ".db")  # Connection to SQLite database
-        # connection, cursor = Data.connection("DB" + ".db")  # Connection to SQLite database
-        # file = Data.openFile(file)  # Open data file
-        # columnNames, lines = Data.getDataARFFile(file)  # Get column names and lines from file
-        # tableName = Data.getTableNameFromARFFFile(lines)
-        # Data.createTableARFF(cursor, tableName, columnNames)  # Create table tableName
-        # Data.insertARFF(tableName, columnNames, lines, cursor, connection)  # Insert data to tableName
+        file = str(input("Please enter the name from arff file:\n"))
+        connection, cursor = Data.connection(file[0:-5] + ".db")  # Connection to SQLite database
+        file = Data.openFile(file)  # Open data file
+        columnNames, lines = Data.getDataARFFile(file)  # Get column names and lines from data file
+        tableName = Data.getTableNameFromARFFFile(lines)  # Get table name from data file
+        Data.createTableARFF(cursor, tableName, columnNames)  # Create table tableName
+        Data.insertARFF(tableName, columnNames, lines, cursor, connection)  # Insert data values to tableName
+
     elif fileOption == 2:
-        file = str(input("Please enter the name from DB SQLite file:\n"))
+        file = str(input("Please enter the name from SQLite file:\n"))
         connection, cursor = Data.connection(file)  # Connection to SQLite database
 
-    tableName = str(input("Table name: \n"))
-    graph, rows = Graph.initializeGraph(tableName, cursor)
+    tableName = str(input("Please enter the table name: \n"))
+    graph, rows = Graph.initializeGraph(tableName, cursor)  # Initialize the graph
 
     option = int(input("Please enter the option of 2-structure you want to create:\n [1] = plain\n [2] = linear\n [3] "
                        "= exponential\n"))
@@ -61,14 +63,22 @@ if __name__ == "__main__":
         optionPlain = int(input("Please enter the option of plain graph you want to create:\n [1] = plain\n [2] = "
                                 "plain with threshold\n"))
         if optionPlain == 1:
-            plainGraph = Graph.createPlainGraph(graph, rows)
+            filename = "plain 2-structure.dot"
+            plainGraph = Graph.createPlainGraph(graph, rows)  # Create a plain graph
         elif optionPlain == 2:
+            filename = "plain 2-structure with threshold.dot"
             threshold = int(input("Please enter the k constant for the threshold:\n"))
-            plainGraph = Graph.createPlainGraphWithThreshold(graph, rows, threshold)
-        Estructura.plain2structure(plainGraph, plainGraph.nodes())
+            plainGraph = Graph.createPlainGraphWithThreshold(graph, rows,
+                                                             threshold)  # Create a plain graph with threshold
+
+        Estructura.plain2structure(plainGraph, plainGraph.nodes(), filename)  # Create a plain 2-structure
+
     elif option == 2:
-        linearGraph = Graph.createLinearGraph(graph, rows)
-        Estructura.linear2structure(linearGraph, linearGraph.nodes())
+        linearGraph = Graph.createLinearGraph(graph, rows)  # Create a linear graph
+        Estructura.linear2structure(linearGraph, linearGraph.nodes(),
+                                    "linear 2-structure.dot")  # Create a linear 2-structure
     elif option == 3:
-        exponentialGraph = Graph.createExponentialGraph(graph, rows)
-        Estructura.exponential2structure(exponentialGraph, exponentialGraph.nodes())
+        exponentialGraph = Graph.createExponentialGraph(graph, rows)  # Create an exponential graph
+        Estructura.exponential2structure(exponentialGraph,
+                                         exponentialGraph.nodes(),
+                                         "exponential 2-structure.dot")  # Create an exponential 2-structure

f80379564fd0a3334a6a71e2061cc7b4ff37efd0
Laura <laura.rodriguez.navas@upc.edu>
2017-03-28 13:59:22 +0200

    s'ha preparat el run_external_program per fer proves de system call amb ubuntu

--- src/graphviz/run_external_program.py
+++ src/graphviz/run_external_program.py
@@ -1,5 +1,5 @@
 """
-This module implements the call for Graphviz program that is associated with a file
+This module implements the call for Graphviz program that is associated with a dot file
 
 Copyright (c) 2016-2017 Laura Rodriguez Navas <laura.rodriguez.navas@upc.edu>
 
@@ -15,11 +15,18 @@ __author__ = 'Laura Rodriguez Navas'
 __license__ = 'MIT'
 
 
-def open_graphviz(program, filename):
-    if sys.platform == 'win32':
+def openGraphviz(filename):
+    """
+    Call the Graphviz program that is associated with a Dot file specified by filename
+
+    :param filename: Dot file
+    :type filename: str
+    """
+    if sys.platform == 'win32':  # Windows platform
         os.startfile(filename)
-    else:
-        subprocess.run(['open', '-a', program, filename])
+    else:  # Linux platform
+        subprocess.call(['open', '-a', 'Graphviz.app', filename])
 
 
-open_graphviz('/Applications/Graphviz.app', 'graphviz.dot')
+if __name__ == '__main__':
+    openGraphviz('graphviz.dot')

7e5da75688e516475ed11db2574c0f4853e74225
laura <laura.rodriguez.navas@upc.edu>
2017-03-30 01:19:31 +0200

    inici implementació d'apriori.py

--- src/data/apriori.py
+++ src/data/apriori.py
@@ -1,23 +1,34 @@
-from src.final.Data import *
+from src.final.Interface import *
 
 
 def convertToSet():
     global s
-    total = list()
+    result = list()
     for line in file.readlines():
         for word in line.split(" "):
             if not word.startswith("("):
                 s.add(word)
-        total.append(s)
+        result.append(frozenset(s))
         print(s)
         s = set()
-    return total
+    return result
 
 
 if __name__ == '__main__':
     s = set()
     file = Data.openFile('test.txt')
-    print(convertToSet())
+    total = convertToSet()
 
-    # for subset in Subset.powersetGenerator(set(total)):
-    #     print(subset)
+    optionData = int(six.moves.input("Please enter the option for the type of file you provide:\n [1] = ARFF\n [2] = "
+                                     "TXT\n [3] = DB\n"))
+
+    columnNames, rows, cursor, tableName = Interface.inputFileOptions(optionData)  # Manages the data entry
+    initGraph, rows = Graph.initGraph(tableName, cursor)  # Initialize the graph
+
+    optionGraph = int(
+        six.moves.input("Please enter the option of graph you want to create:\n [1] = plain\n [2] = plain "
+                        "with threshold\n [3] = linear\n [4] = exponential\n"))
+    graph = Interface.graphOptions(optionGraph, initGraph, rows)  # Create a type of graph
+
+    clansList = Clan.clans(graph, total)  # Create clans list
+    print(clansList)

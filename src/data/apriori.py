1b9afb779e33307b96044645f522498bdbfb60d5
laura <laura.rodriguez.navas@upc.edu>
2017-03-30 00:57:29 +0200

    nova funcionalitat que converteix el fitxer resultant de l'algoritme de priori en un conjunts

--- src/data/apriori.py
+++ src/data/apriori.py
@@ -1,9 +1,23 @@
 from src.final.Data import *
-from src.final.Subset import *
+
+
+def convertToSet():
+    global s
+    total = list()
+    for line in file.readlines():
+        for word in line.split(" "):
+            if not word.startswith("("):
+                s.add(word)
+        total.append(s)
+        print(s)
+        s = set()
+    return total
+
 
 if __name__ == '__main__':
-    file = Data.openFile('cpu.fq')
-    for line in file.readlines():
-        print(line[0], line[1])
+    s = set()
+    file = Data.openFile('test.txt')
+    print(convertToSet())
 
-    Subset.powersetGenerator()
+    # for subset in Subset.powersetGenerator(set(total)):
+    #     print(subset)

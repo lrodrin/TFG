29d21e0710b11014355bd0d8720af0f2d210ead5
Laura <laura.rodriguez.navas@upc.edu>
2017-04-02 14:39:23 +0200

    s'ha millorat /src/data/create_data_from_arff/txt per generar taules sql més ràpid i provar diferents jocs de prova

---
+++ src/data/parse.py
@@ -0,0 +1,13 @@
+"""
+This module implements rules for parse TXT files
+
+Copyright (c) 2016-2017 Laura Rodriguez Navas <laura.rodriguez.navas@upc.edu>
+
+Distributed under MIT license
+[https://opensource.org/licenses/MIT]
+"""
+
+__author__ = 'Laura Rodriguez Navas'
+__license__ = 'MIT'
+
+# TODO replace char - for _ in TXT file because SQLite not accept this char

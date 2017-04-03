505e4669e7ac720ec61a25e6af4e62a38e349356
Laura <laura.rodriguez.navas@upc.edu>
2017-04-03 17:58:08 +0200

    reformat code

--- src/final/mainSubset.py
+++ src/final/mainSubset.py
@@ -22,7 +22,8 @@ if __name__ == "__main__":
 
     optionData = int(six.moves.input("Please enter the option for the type of file you provide:\n [1] = ARFF\n [2] = "
                                      "TXT\n"))
-    filename = str(input("Please enter the file name you provide:\n"))
-    moreFrequentSubsets = Subset.moreFrequentSubsets(filename, optionData, 1)  # The more frequents subsets from
-    # filename
+    filename = str(six.moves.input("Please enter the file name you provide:\n"))
+    probability = float(six.moves.input("Please enter the probability for the more frequent subsets creation:\n"))
+    moreFrequentSubsets = Subset.moreFrequentSubsets(filename, optionData, probability)  # The more frequents subsets
+    #  from filename
     print(moreFrequentSubsets)

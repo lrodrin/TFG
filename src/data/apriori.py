43921e3f25ce525726ab35264cc4da87c1ce4d20
Laura <laura.rodriguez.navas@upc.edu>
2017-04-29 11:28:03 +0200

    backup

--- src/data/apriori.py
+++ src/data/apriori.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     optionData = int(six.moves.input("Please enter the option for the type of file you provide:\n [1] = ARFF\n [2] = "
                                      "TXT\n"))
     filename = str(six.moves.input("Please enter the file name you provide:\n"))
-    probability = float(six.moves.input("Please enter the probability for the more frequent subsets creation:\n"))
-    moreFrequentSubsets = Subset.moreFrequentSubsets(filename, optionData, probability)  # The more frequents subsets
+    support = float(six.moves.input("Please enter the probability for the more frequent subsets creation:\n"))
+    moreFrequentSubsets = Subset.moreFrequentSubsets(filename, optionData, support)  # The more frequents subsets
     #  from filename
     print(moreFrequentSubsets)

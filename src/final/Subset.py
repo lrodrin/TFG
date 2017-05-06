9539340fd84ab80936c1c42980ef2ae18cc1cb96
laura <laura.rodriguez.navas@upc.edu>
2017-05-06 19:05:48 +0200

    crida a apriori des de OS X implementada

--- src/final/Subset.py
+++ src/final/Subset.py
@@ -60,7 +60,9 @@ class Subset:
             elif sys.platform == 'linux2':  # Linux platform
                 os.system("./apriori -s{0} -C'@%' {1} {2}".format(str(support), str(dataFile.replace("\n", "")),
                                                                   str(newFilename.replace("\n", ""))))
-                # elif sys.platform == 'darwin': # Mac platform
+            elif sys.platform == 'darwin':  # Mac platform
+                os.system("./aprioriOSX -s{0} -C'@%' {1} {2}".format(str(support), str(dataFile.replace("\n", "")),
+                                                                   str(newFilename.replace("\n", ""))))
 
         print(newFilename)
         filename = (Data.openFile(newFilename.replace("\n", "")))  # Open AP data file
